7429b4ae108bac037c4ed15949b079305b0284e1
Ugendu Martins Ositadinma <ugendu04@gmail.com>
2016-10-12 22:02:56 +0100

    Update ioc.sh

--- ioc.sh
+++ ioc.sh
@@ -52,14 +52,7 @@ namespace App\Repositories\\${repoName};
 
 interface ${repoName}Contract
 {
-	public function index();
-	public function create();
-	public function store(Request \$request);
-	public function show(\$id);
-	public function edit(\$id);
-	public function update(Request \$request, \$id);
-	public function destroy(\$id);
-
+	//
 }" > ./app/Repositories/${repoName}/${repoName}${contract}
 }
 writeContractBoilerPlate
@@ -72,66 +65,7 @@ namespace App\Repositories\\${repoName};
 class ${eloquent}${repoName}Repository implements ${repoName}Contract
 {
 
-	public function __construct()
-	{
-		//
-	}
-
-	/**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request \$request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(\$id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(\$id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request \$request, \$id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(\$id)
-    {
-        //
-    }
+	//
 }" > ./app/Repositories/${repoName}/${eloquent}${repoName}${repository}
 }
 writeRepositoryBoilerPlate
